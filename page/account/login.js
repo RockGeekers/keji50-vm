e45c75a5feacc2ce0dddfa49e5f72080006c7517
sophia <sophiaytyt@gmail.com>
2016-01-24 13:49:56 +0800

    fix

---
+++ page/account/login.js
@@ -0,0 +1,22 @@
+var $ = require('jquery');
+$('#J_submit').click(function(){
+	$('#J_errorwrap').hide();
+	$.post('/account/ajax/sign_in', $('#loginForm').serialize(), function(data) {
+		if(data.code == 0){
+			location.href = '#'
+		}else{
+			$('#J_errorwrap').show().find('.error-msg').text(data.message);
+		}
+	});
+	return false;
+});
+$("#J_regsub").click(function() {
+	$.post('/account/ajax/sign_up', $('#regForm').serialize(), function(data) {
+		if(data.code == 0){
+			location.href = '#'
+		}else{
+			$('#J_errorwrap').show().find('.error-msg').text(data.message);
+		}
+	});
+	return false;
+});
